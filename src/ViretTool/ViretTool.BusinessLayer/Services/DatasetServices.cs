5b06f533c35b99eaf3bacbd201d53f0967a421c7
Premysl Cech <cech@ksi.mff.cuni.cz>
2018-12-09 13:45:01 +0100

    One forgotten reference - all services will be held in DatasetServices class

--- src/ViretTool/ViretTool.BusinessLayer/Services/DatasetServices.cs
+++ src/ViretTool/ViretTool.BusinessLayer/Services/DatasetServices.cs
@@ -1,14 +1,17 @@
-﻿using ViretTool.BusinessLayer.Thumbnails;
+﻿using ViretTool.BusinessLayer.Datasets;
+using ViretTool.BusinessLayer.Thumbnails;
 
 namespace ViretTool.BusinessLayer.Services
 {
     public class DatasetServices
     {
-        public DatasetServices(IThumbnailService<Thumbnail<byte[]>> thumbnailService)
+        public DatasetServices(IThumbnailService<Thumbnail<byte[]>> thumbnailService, IDatasetService datasetService)
         {
             ThumbnailService = thumbnailService;
+            DatasetService = datasetService;
         }
 
         public IThumbnailService<Thumbnail<byte[]>> ThumbnailService { get; }
+        public IDatasetService DatasetService { get; }
     }
 }
