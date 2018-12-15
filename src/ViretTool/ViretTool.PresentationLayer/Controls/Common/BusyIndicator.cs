0971ac7b4167a79b281ca36102d58a4f5be552ff
Premysl Cech <cech@ksi.mff.cuni.cz>
2018-12-15 11:57:55 +0100

    Busy indicator generalized

---
+++ src/ViretTool/ViretTool.PresentationLayer/Controls/Common/BusyIndicator.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ViretTool.PresentationLayer.Controls.Common
+{
+    public class BusyIndicator : ContentControl
+    {
+
+        public static readonly DependencyProperty IsBusyProperty = DependencyProperty.Register(
+            nameof(IsBusy),
+            typeof(bool),
+            typeof(BusyIndicator),
+            new FrameworkPropertyMetadata(default(bool)) { BindsTwoWayByDefault = true });
+
+        public bool IsBusy
+        {
+            get => (bool)GetValue(IsBusyProperty);
+            set => SetValue(IsBusyProperty, value);
+        }
+    }
+}
