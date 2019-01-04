a46726dec75e30b045de29c1af842551d94e8751
Gregor Kovalcik <gregor.kovalcik@gmail.com>
2019-01-04 11:51:27 +0100

    Added missing IColorSketchModel interface.

---
+++ src/ViretTool/ViretTool.BusinessLayer/RankingModels/Similarity/Models/IColorSketchModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViretTool.BusinessLayer.RankingModels.Queries;
+
+namespace ViretTool.BusinessLayer.RankingModels.Similarity.Models
+{
+    public interface IColorSketchModel : ISimilarityModel<ColorSketchQuery>
+    {
+    }
+}
