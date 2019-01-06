7a83953f97e1d8c9bb78913bf6d303934b6895e8
Premysl Cech <cech@ksi.mff.cuni.cz>
2019-01-06 15:13:35 +0100

    Unhandled exceptions are logged

--- src/ViretTool/ViretTool/App.xaml.cs
+++ src/ViretTool/ViretTool/App.xaml.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
+using Castle.Services.Logging.NLogIntegration;
+using ILogger = Castle.Core.Logging.ILogger;
 
 namespace ViretTool
 {
@@ -13,5 +11,22 @@ namespace ViretTool
     /// </summary>
     public partial class App : Application
     {
+        private readonly ILogger _logger;
+
+        public App()
+        {
+            NLogFactory nLogFactory = new NLogFactory(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
+            _logger = nLogFactory.Create(GetType());
+            DispatcherUnhandledException += CurrentDomainOnUnhandledException;
+        }
+
+        private void CurrentDomainOnUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            _logger.Fatal("Unhandled Exception: ", e.Exception);
+            MessageBox.Show(e.Exception.Message, "Fatal error");
+            
+            e.Handled = true;
+            Environment.Exit(-1);
+        }
     }
 }
