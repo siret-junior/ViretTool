d28c72c9fed01b835e400f99524bcdda1f4589a0
Gregor Kovalcik <gregor.kovalcik@gmail.com>
2019-01-08 10:05:06 +0100

    Added storing submission log to local file.

--- src/ViretTool/ViretTool.BusinessLayer/Submission/SubmissionService.cs
+++ src/ViretTool/ViretTool.BusinessLayer/Submission/SubmissionService.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.IO;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using ViretTool.BusinessLayer.ActionLogging;
@@ -8,6 +10,7 @@ namespace ViretTool.BusinessLayer.Submission
     public class SubmissionService : ISubmissionService
     {
         private const string BaseUrl = "http://demo2.itec.aau.at:80/vbs/submit";
+        private const string SUBMISSION_LOG_DIRECTORY = "SubmissionLogs";
         private readonly HttpClient _client = new HttpClient();
         private readonly IInteractionLogger _interactionLogger;
 
@@ -19,14 +22,36 @@ namespace ViretTool.BusinessLayer.Submission
         public async Task<string> SubmitFrameAsync(FrameToSubmit frameToSubmit)
         {
             string url = GetUrl(_interactionLogger.Log.TeamId, _interactionLogger.Log.MemberId, frameToSubmit);
-            StringContent content = new StringContent(GetContent(), Encoding.UTF8, "application/json");
+            string jsonInteractionLog = GetContent();
+            StringContent content = new StringContent(jsonInteractionLog, Encoding.UTF8, "application/json");
             HttpResponseMessage response = await _client.PostAsync(url, content);
+
+            // log to disk
+            StoreSubmissionLog(url, jsonInteractionLog);
+
             if (response.IsSuccessStatusCode)
             {
                 _interactionLogger.ResetLog();
             }
-
+            
             return await response.Content.ReadAsStringAsync();
+        }
+
+        private void StoreSubmissionLog(string url, string jsonInteractionLog)
+        {
+            try
+            {
+                Directory.CreateDirectory(SUBMISSION_LOG_DIRECTORY);
+                string filename = 
+                    "Submission_" 
+                    + Environment.MachineName 
+                    + DateTime.UtcNow.ToString("s");
+                File.WriteAllLines(filename, new string[] { url, jsonInteractionLog });
+            }
+            catch (Exception ex)
+            {
+                //
+            }
         }
 
         public async Task<string> SubmitLog()
