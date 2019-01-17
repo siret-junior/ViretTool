d9742f86aabcbbd06eb11c3d0369f3279fbacaae
Premysl Cech <cech@ksi.mff.cuni.cz>
2019-01-17 12:48:53 +0100

    Castle console installator

---
+++ src/ViretTool/KisQueryResultExtraction/ArgumentPassingDependencyResolver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Castle.MicroKernel.Context;
+using Castle.MicroKernel.Resolvers;
+
+namespace KisQueryResultExtraction
+{
+    /// <summary>
+    /// Resolver, ktery zajistuje propagovani parametru pri resolvovani (CW defaultne posila parametry jen do rootu).
+    /// http://stackoverflow.com/questions/3904951/castle-windsor-ioc-passing-constructor-parameters-to-child-components
+    /// </summary>
+    public class ArgumentPassingDependencyResolver : DefaultDependencyResolver
+    {
+        protected override CreationContext RebuildContextForParameter(CreationContext current, Type parameterType)
+        {
+            if (parameterType.ContainsGenericParameters)
+            {
+                // this behaviour copied from base class
+                return current;
+            }
+
+            // the difference in the following line is that "true" is passed
+            // instead of "false" as the third parameter
+            return new CreationContext(parameterType, current, true);
+        }
+    }
+}
--- src/ViretTool/KisQueryResultExtraction/Program.cs
+++ src/ViretTool/KisQueryResultExtraction/Program.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Castle.Facilities.TypedFactory;
+using Castle.MicroKernel;
+using Castle.MicroKernel.Proxy;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using ViretTool.BusinessLayer.Services;
 
 namespace KisQueryResultExtraction
 {
@@ -12,6 +18,17 @@ namespace KisQueryResultExtraction
         {
             // string inputDirectory = args[0];
             // want to use IBiTemporalRankingService here, initialized by inputDirectory
+
+            using (WindsorContainer container = new WindsorContainer(new DefaultKernel(new ArgumentPassingDependencyResolver(), new NotSupportedProxyFactory()), new DefaultComponentInstaller()))
+            {
+                container.AddFacility<TypedFactoryFacility>();
+                container.Install(FromAssembly.This());
+
+                IDatasetServicesManager datasetServiceManager = container.Resolve<IDatasetServicesManager>();
+                datasetServiceManager.OpenDataset("...");   //TODO
+                //compute result
+                //datasetServiceManager.CurrentDataset.RankingService.ComputeRankedResultSet()
+            }
         }
     }
 }
