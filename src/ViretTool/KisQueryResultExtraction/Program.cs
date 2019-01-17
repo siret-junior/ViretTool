bbc1492e9dfef1c2b7f7cc30fed8c3686a89c62f
Gregor Kovalcik <gregor.kovalcik@gmail.com>
2019-01-17 12:38:17 +0100

    Initial project setup for KisQueryResultExtraction.

---
+++ src/ViretTool/KisQueryResultExtraction/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KisQueryResultExtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string inputDirectory = args[0];
+            // want to use IBiTemporalRankingService here, initialized by inputDirectory
+        }
+    }
+}
