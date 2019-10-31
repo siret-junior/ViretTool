51cc582a907d0dd9d7e8a793efdf7d8741a6e0a8
Gregor Kovalcik <gregor.kovalcik@gmail.com>
2019-10-31 18:38:17 +0100

    DataIO refactoring.

--- src/ViretTool/ViretTool.DataLayer/DataIO/FileFormatUtilities.cs
+++ src/ViretTool/ViretTool.DataLayer/DataIO/FileFormatUtilities.cs
@@ -1,14 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ViretTool.DataLayer.DataIO
 {
+    /// <summary>
+    /// Helper routines used to check file formats.
+    /// </summary>
     public class FileFormatUtilities
     {
+        /// <summary>
+        /// Checks whether the value is in defined range (including border values).
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="name">Name of the value, used in exception message.</param>
+        /// <param name="value">The value that is being tested.</param>
+        /// <param name="minValue">Lower bound for the tested value (including this value).</param>
+        /// <param name="maxValue">Upper bound for the tested value (including this value).</param>
         public static void CheckValueInRange<T>(string name, T value, T minValue, T maxValue) where T : IComparable
         {
             if (value.CompareTo(minValue) < 0 || value.CompareTo(maxValue) > 0)
@@ -17,6 +24,14 @@ namespace ViretTool.DataLayer.DataIO
             }
         }
 
+        /// <summary>
+        /// Checks every item value in the input array whether it is in the defined range (including border values).
+        /// </summary>
+        /// <typeparam name="T">IComparable type of the array item values.</typeparam>
+        /// <param name="name">Name of the input array, used in exception message.</param>
+        /// <param name="values">An array of values to be tested.</param>
+        /// <param name="minValue">Lower bound for the tested array items (including this value).</param>
+        /// <param name="maxValue">Upper bound for the tested array items (including this value).</param>
         public static void CheckValuesInRange<T>(string name, T[] values, T minValue, T maxValue) where T : IComparable
         {
             for (int i = 0; i < values.Length; i++)
@@ -26,6 +41,13 @@ namespace ViretTool.DataLayer.DataIO
             }
         }
         
+        /// <summary>
+        /// Checks whether the values in the input array are incrementing.
+        /// Consecutive values can not be the same value, they have to increment.
+        /// </summary>
+        /// <typeparam name="T">IComparable type of the array items.</typeparam>
+        /// <param name="name">Name of the input array, used in exception message.</param>
+        /// <param name="values">Input array to be tested.</param>
         public static void CheckValuesIncrement<T>(string name, T[] values) where T : IComparable
         {
             if (values.Length > 0)
