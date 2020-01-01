c82a2c04ceb8b1db17aaa962dd16c0a0e4e044ba
Gregor Kovalcik <gregor.kovalcik@gmail.com>
2020-01-01 15:39:58 +0100

    An attempt to use WPF binding.

--- src/ViretTool/ViretTool.PresentationLayer/Controls/Common/TranscriptFilter/TranscriptFilterViewModel.cs
+++ src/ViretTool/ViretTool.PresentationLayer/Controls/Common/TranscriptFilter/TranscriptFilterViewModel.cs
@@ -3,11 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Caliburn.Micro;
 
 namespace ViretTool.PresentationLayer.Controls.Common.TranscriptFilter
 {
     public class TranscriptFilterViewModel : PropertyChangedBase
     {
+        private string _inputText;
+        public string InputText 
+        {
+            get => _inputText;
+            set
+            {
+                if (_inputText == value)
+                {
+                    return;
+                }
+
+                _inputText = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+
+        public void Execute(KeyEventArgs keyArgs)
+        {
+            if (keyArgs.Key == Key.Enter)
+            {
+                // Do Stuff
+            }
+        }
     }
 }
