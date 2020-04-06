d5347fb6f60757528ca10b6595c22644d6fab78c
Vít Škrhák <vitek.skrhak@seznam.cz>
2020-04-06 15:15:13 +0200

    Debbuging - Program doesn't get killed when new query is requested even though SOMDisplay from previous query is not ready yet.

--- src/ViretTool/ViretTool.PresentationLayer/Windows/ViewModels/MainWindowViewModel.cs
+++ src/ViretTool/ViretTool.PresentationLayer/Windows/ViewModels/MainWindowViewModel.cs
@@ -722,7 +722,7 @@ namespace ViretTool.PresentationLayer.Windows.ViewModels
                 //_sortingTask = _gridSorter.GetSortedFrameIdsAsync(sortedIds.Take(TopFramesCount).ToList(), DetailViewModel.ColumnCount, _cancellationTokenSource);
                 
                 // TODO: consider catching and logging LoadSomDisplay exceptions.
-                _ = Task.Factory.StartNew(() => LoadSomDisplay(sortedIds), TaskCreationOptions.LongRunning);
+                _ = Task.Factory.StartNew(() => LoadSomDisplay(sortedIds));
                 
 
                 await QueryResults.LoadFramesForIds(sortedIds);
@@ -736,10 +736,14 @@ namespace ViretTool.PresentationLayer.Windows.ViewModels
                 IsBusy = false;
             }
         }
-        private async void LoadSomDisplay(IList<int> sortedIds)
+        private object _somLock = new object();
+        private void LoadSomDisplay(IList<int> sortedIds)
         {
             IsSomDisplayLoaded = false;
-            await SomDisplay.LoadFramesForIds(sortedIds);
+            lock(_somLock)
+            {
+                SomDisplay.LoadFramesForIds(sortedIds);
+            }
             IsSomDisplayLoaded = true;
             
         }
