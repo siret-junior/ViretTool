b05fe324f9e0776770b760d3fc9d43a2fe761640
Vít Škrhák <vitek.skrhak@seznam.cz>
2021-06-06 15:18:46 +0200

    Implementation of communication with server

--- src/ViretTool/ViretCore/Ranking/W2VV/TextToVectorRemote.cs
+++ src/ViretTool/ViretCore/Ranking/W2VV/TextToVectorRemote.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -43,9 +44,55 @@ namespace Viret.Ranking.W2VV
 
         public /* async? */ float[] TextToVector(string[] query)
         {
-            float[] vector = new float[VectorDimension]; // TODO: convert query to vector
-            vector = _pcaConversion.ApplyPCA(vector);
-            return vector;
+            if (query.Length < 1)
+            {
+                float[] vector = new float[VectorDimension]; // TODO: convert query to vector
+                vector = _pcaConversion.ApplyPCA(vector);
+                return vector;
+            }
+            //my%20text%20query
+            StringBuilder URLBuilder = new StringBuilder();
+
+            // TODO: add actual server url
+            // --- HERE ADD ACTUAL SERVER URL ---//
+            string serverUrl = "http://195.113.18.36:42013/";
+
+            URLBuilder.Append(serverUrl);
+
+            string service = "clip/"; // change to "bert/" if BERT is needed
+
+            URLBuilder.Append(service);
+            // --- END OF "HERE ADD ACTUAL SERVER URL" ---//
+
+            bool firstKeyword = true;
+            foreach (string keyword in query)
+            {
+                if (!firstKeyword)
+                    URLBuilder.Append("%20");
+                else
+                    firstKeyword = false;
+
+                URLBuilder.Append(keyword);
+            }
+
+            string url = URLBuilder.ToString();
+
+            WebClient webClient = new WebClient();
+
+            byte[] responseData = webClient.DownloadData(url);
+
+            // TODO: Endianness?
+            float[] responseVector = new float[VectorDimension];
+
+            // any more elegant way to convert byte[2560] to float[640]?
+            for(int i = 0; i < VectorDimension; i++)
+            {
+                responseVector[i] = BitConverter.ToSingle(responseData, i * 4);
+            }
+            
+            float[] resultVector = _pcaConversion.ApplyPCA(responseVector);
+
+            return resultVector;
         }
     }
 }
